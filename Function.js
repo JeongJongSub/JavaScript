ae150b6686a06a6abd4de011826a4c18fcabdb6b
JeongJongSub <jeongjs501@gmail.com>
2025-04-29 00:07:16 +0900

    "혼자 공부하는 자바스크립트"

---
+++ Function.js
@@ -0,0 +1,38 @@
+// if문
+
+let num = 10;
+
+
+if (num < 10) {
+    console.log('num은 10보다 작습니다')
+} else if (num > 10) {
+    console.log('num은 10보다 큽니다.')
+
+} else {
+    console.log('num은 10입니다.')
+
+}
+
+// switch case 문
+
+let fruit =  'apple';
+
+switch(fruit){
+    case 'banana' :
+        console.log('바나나');
+        break;
+    case 'orange' :
+        console.log('오렌지');
+        break;
+    case 'apple' :
+        console.log('사과');
+        break;
+    case 'grape' :
+        console.log('포도');
+        break;
+    default :
+        console.log('다른과일')
+}
+
+
+
